48eb7caaabcd92436e9db0bbe1d5384e23b0cf52
Mustafa Kemal GÖKÇE <m.kemalgokce10@gmail.com>
2019-05-12 14:56:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include"odev9.h"
 //hahahahaadad
-//yarrak kafalý coz sunu artýk sikerim boyle odevi
+//yarrak kafalý coz sunu artýk sikerim boyle odevi(memet)
 int main() {
 	
 	AkilliDizi<int> a(2);
