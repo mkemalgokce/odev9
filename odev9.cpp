c45a5f6216f23d7db698fd29aadd80d5e7a275dc
mkemalgokce <46056478+mkemalgokce@users.noreply.github.com>
2019-05-12 14:23:13 +0300

    Update odev9.cpp

--- odev9.cpp
+++ odev9.cpp
@@ -1,54 +1,49 @@
 
-#include<iostream>
 #include"odev9.h"
-#include<vector>
 
-using namespace std;
-template<typename T>
-ostream & operator<<(ostream& out, const AkilliDizi<T>& other) {
-	for (int i = 0; i < other.elemanSayisi; ++i)
-		out << i << " ";
 
-	cout << endl;
-}
-template<typename T>
+template<class T>
 AkilliDizi<T>::AkilliDizi() {
 	veri = nullptr;
 	elemanSayisi = 0;
 	kapasite = 0;
-
+}
+template<class T>
+AkilliDizi<T>::AkilliDizi (T i) {
+	elemanSayisi = 1;
+	kapasite = elemanSayisi;
+	veri = new T[elemanSayisi];
+	if (veri == nullptr)
+		exit(-1);
+	veri[0] = i;
 	
 
-}
-template<typename T>
-AkilliDizi<T>::AkilliDizi (T i) {
-	kapasite = elemanSayisi;
-	veri = new T(kapasite);
-	veri[0] = i;
-	elemanSayisi = 1;
-
 
 }
-template<typename T>
+template<class T>
 AkilliDizi<T>::AkilliDizi(const T* intDizi, unsigned int elemanSayisi) {
 
 	this->elemanSayisi = elemanSayisi;
 	kapasite = elemanSayisi;
-	veri = new T(kapasite);
+	veri = new T[this->elemanSayisi];
+	if (veri == nullptr)
+		exit(-1);
 	for (int i = 0; i < elemanSayisi; ++i)
 		veri[i] = intDizi[i];
 }
-template<typename T>
+template<class T>
 AkilliDizi<T>::AkilliDizi(const vector<T>& intVector) {
 
 	intVector.size() = elemanSayisi;
 	kapasite = elemanSayisi;
-	veri = new T(kapasite);
+	veri = new T[elemanSayisi];
+	if (veri == nullptr)
+		exit(-1);
 	for (int i = 0; i < elemanSayisi; ++i)
 		veri[i] = intVector[i];
 
 }
-template<typename T>
+template<class T>
 AkilliDizi<T>::~AkilliDizi(){
 	
 	if (veri != nullptr)
@@ -56,38 +51,42 @@ AkilliDizi<T>::~AkilliDizi(){
 	
 
 }
-template<typename T>
+template<class T>
 AkilliDizi<T>::AkilliDizi(const AkilliDizi<T>& other) {
 	elemanSayisi = other.elemanSayisi;
 	kapasite = other.kapasite;
 	veri = nullptr;
-	delete[] veri;
-	veri = new T(kapasite);
+	if(elemanSayisi=!0){
+	veri = new T[kapasite];
+	if (veri == nullptr)
+		exit(-1);
 	for (int i = 0; i < elemanSayisi; ++i)
 		veri[i] = other.veri[i];
 
-	
+	}
 }
-template<typename T>
+template<class T>
 const AkilliDizi<T>& AkilliDizi<T>::operator=(const AkilliDizi& other) {
-	if (this = other)
+	if (this == other)
 		return *this;
 	if (veri != nullptr)
 		delete[] veri;
 	elemanSayisi = other.elemanSayisi;
 	kapasite = other.kapasite;
-	veri = new T(kapasite);
+	veri = new T[kapasite];
+	if (veri == nullptr)
+		exit(-1);
 	for (int i = 0; i < elemanSayisi; ++i)
 		veri[i] = other.veri[i];
 	return *this;
 }
-template<typename T>
+template<class T>
 void AkilliDizi<T>::operator[](T verilenDeger) {
-	if (T > elemanSayisi)
-		cout << "Hata , verilen deðer eleman sayýsýndan buyuk!" << endl;
+	if (verilenDeger > elemanSayisi)
+		cout << "Hata , verilen deÄŸer eleman sayÄ±sÄ±ndan buyuk!" << endl;
 
 }
-template<typename T>
+template<class T>
 AkilliDizi<T>& AkilliDizi<T>::operator +(T verilenDeger) {
 	vector<T> x;
 	for (int i = 0; i < elemanSayisi; ++i) {
@@ -95,7 +94,7 @@ AkilliDizi<T>& AkilliDizi<T>::operator +(T verilenDeger) {
 	}
 	if (veri != nullptr)
 		delete[] veri;
-	veri = new T(elemanSayisi + 1);
+	veri = new T[elemanSayisi + 1];
 	for (int i = 0; i < elemanSayisi; ++i) {
 		veri[i] = x[i];
 	}
@@ -124,3 +123,11 @@ void AkilliDizi<T>::sirala() {
 	}
 
 }
+template<class T>
+ostream& operator << (ostream& out, const AkilliDizi<T>& dizi)
+{
+	for (unsigned i = 0; i < dizi.elemanSayisi; i++)
+		out << dizi.veri[i] << " ";
+	return out;
+}
+
